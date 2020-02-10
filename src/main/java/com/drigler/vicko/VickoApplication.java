f62ab6dca9f1a479595360e99a58c0c7a7510b74
Damir Rigler <damir.rigler@gmail.com>
2020-02-10 18:49:36 +0100

    initial commit

---
+++ src/main/java/com/drigler/vicko/VickoApplication.java
@@ -0,0 +1,13 @@
+package com.drigler.vicko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VickoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VickoApplication.class, args);
+	}
+
+}
