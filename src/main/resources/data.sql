9a279ac5cb8159b50a3f5e2ba4f2b52fdb04c0bd
Damir Rigler <damir.rigler@gmail.com>
2020-02-12 23:01:49 +0100

    updated data config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
 insert into jokes.category (id, name) values (1, 'programming'), (2, 'one liner'), (3, 'dog') ON CONFLICT DO NOTHING;
-insert into jokes.joke (id_category, content) values
-    (3, 'In a park people come across a man playing chess against a dog. They were astonished and said: "What a clever dog!" But the man protested and replied: "No, no, he isn''t that clever. I''m leading by three games to one!"'), (1, ' My wifi suddenly stop working then I realized that my neighbors have not paid the bill.    How irresponsible people are.'),
-    (1, 'If at first you don''t succeed, call it version 1.0.'),
-    (2, ' One day YouTube, Twitter and Facebook will merge and be known as YouTwitFace :)'),
-    (2, 'If life is handing you melons... You just might be dyslexic.')
+insert into jokes.joke (id, id_category, content) values
+    (0, 3, 'In a park people come across a man playing chess against a dog. They were astonished and said: "What a clever dog!" But the man protested and replied: "No, no, he isn''t that clever. I''m leading by three games to one!"'), 
+    (1, 1, ' My wifi suddenly stop working then I realized that my neighbors have not paid the bill. How irresponsible people are.'),
+    (2, 1, 'If at first you don''t succeed, call it version 1.0.'),
+    (3, 2, ' One day YouTube, Twitter and Facebook will merge and be known as YouTwitFace :)'),
+    (4, 2, 'If life is handing you melons... You just might be dyslexic.')
 ON CONFLICT DO NOTHING;
